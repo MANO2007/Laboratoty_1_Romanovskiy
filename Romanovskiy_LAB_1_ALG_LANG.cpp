62130e4c2eeadd09319139ce2106ee523e679393
MANO2007 <dsromanovsky@yandex.ru>
2026-09-15 11:42:51 +0300

    commit ready console

--- Romanovskiy_LAB_1_ALG_LANG.cpp
+++ Romanovskiy_LAB_1_ALG_LANG.cpp
@@ -1,29 +1,81 @@
-﻿// Romanovskiy_LAB_1_ALG_LANG.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
-
+using namespace std;
 
 int main()
 {
-    int x;
-    std::cin >> x;
-    if (x != 0) {
-        std::cout << 10 / x;
+    int x = 1;
+    while (x != 0) {
+        cout << "Enter a digit from 0 to 7, where each digit corresponds to: \n1. Add pipe\n 2. Add compressor station\n 3. View all objects\n 4. Edit pipe\n 5. Edit compressor station\n 6. Save\n 7. Load\n 0. Exit\n";
+        cin >> x;
+        switch (x) {
+        case 1:
+            cout << "\n";
+            break;
+        case 2:
+            cout << "\n";
+            break;
+        case 3:
+            cout << "\n";
+            break;
+        case 4:
+            cout << "\n";
+            break;
+        case 5:
+            cout << "\n";
+            break;
+        case 6:
+            cout << "\n";
+            break;
+        case 7:
+            cout << "\n";
+            break;
+        case 0:
+            cout << "\n";
+            break;
+        }
     }
-    else {
-        std::cout << "eror";
-    }
+
+
     
 }
+struct Pipe
+{
+    string mark;
+    double length;
+    double diametr;
+    string sign;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+};
+struct CompressorStation
+{
+    string name;
+    int count_shops;
+    int count_shops_in_work;
+    char station_klass;
+};
+void AddPipe()
+{
+
+};
+void AddCompressorStation()
+{
+
+};
+void ShowAllObjects() {
+
+};
+void ChangePipe() {
+
+};
+void ChangeCompessorStation() {
+
+};
+void Save() {
+
+};
+void Load() {
+
+};
